2ce5cbbebd8234339188d3a9bc10f77689199d49
tryou <tryou@GH>
2021-10-08 21:18:22 +0200

    clean

--- cloudchains_pks_extractor.py
+++ cloudchains_pks_extractor.py
@@ -5,7 +5,7 @@ import json
 import requests
 
 # configurable settings >>
-only_funded_address = 1
+only_funded_address = True
 # configurable settings <<
 
 username = os.getlogin()
@@ -21,33 +21,27 @@ elif system == "Linux":
 elif system == "Darwin":
     path = "/Users/" + username + "/Library/Application Support/CloudChains/settings"
 else:
-    print("no valid os detected, exiting")
+    print("no valid os detected, close script")
     exit()
 
 
 def instruct_wallet(method, params=[]):
-    # print("instruct_wallet(", method, ",", params, ")")
     url = "http://127.0.0.1:" + str(rpc_port) + "/"
     payload = json.dumps({"method": method, "params": params})
     headers = requests.utils.default_headers()
     auth = (rpc_user, rpc_password)
     try:
         response = json.loads(requests.request("POST", url, data=payload, headers=headers, auth=auth).text)['result']
-    except:
-        print('No response from wallet, check xlite/cc is running on this machine')
+    except Exception as e:
+        print("instruct_wallet(", method, ",", params, ")")
+        print(type(e), e)
         response = None
     return response
 
 
 def list_pks():
     if rpc_port > 0:
-        if only_funded_address == 0:
-            get_addresses = instruct_wallet('getaddressesbyaccount', ["main"])
-            if get_addresses:
-                for address in get_addresses:
-                    privkey = instruct_wallet('dumpprivkey', [address])
-                    print(address, ":", privkey)
-        else:
+        if only_funded_address:
             list_unspent = instruct_wallet('listunspent', [])
             if list_unspent:
                 last = ""
@@ -55,12 +49,19 @@ def list_pks():
                     if each['address'] != last:
                         print(each['address'], ":", instruct_wallet('dumpprivkey', [each['address']]))
                     last = each['address']
+        else:
+            get_addresses = instruct_wallet('getaddressesbyaccount', ["main"])
+            if get_addresses:
+                for address in get_addresses:
+                    privkey = instruct_wallet('dumpprivkey', [address])
+                    print(address, ":", privkey)
 
 
 if __name__ == "__main__":
-    print("formatage: \n\nCOIN\naddress : privatekey\n")
+    print("only_funded_address:", only_funded_address)
+    print("\nformatage: \n\nCOIN\naddress : privatekey\n")
     for filename in glob.glob(os.path.join(path, '*.json')):
-        if "config-master.json" not in filename:
+        if "config-master.json" not in filename and "null" not in filename:
             coin = filename[filename.find("-") + 1:-5]
             f = open(filename)
             config = json.load(f)
